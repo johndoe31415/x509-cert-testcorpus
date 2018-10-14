a60c60f83ee19aacc84bf61acdd97f4350a9103c
Johannes Bauer <joe@johannes-bauer.com>
2018-10-14 20:31:09 +0200

    Better progress indicator
    Instead of the stupid idea I had five minutes ago, just do it properly
    and have a normal progress indicator by counting how many of the list
    have already been processed.

--- cert_scrape.py
+++ cert_scrape.py
@@ -15,14 +15,14 @@ import threading
 thread_cnt = 12
 threads = threading.Semaphore(thread_cnt)
 gracetime = 0.5
-#max_entries = 10000; randomized = False
-max_entries = None; randomized = True
+#max_entries = 10000
+max_entries = None
 Entry = collections.namedtuple("Entry", [ "tld", "key", "outfile", "crtfile" ])
 ctr = collections.Counter()
 
 entries = [ ]
 with open("top-1m.csv") as f:
-	for line in csv.reader(f):
+	for (lineno, line) in enumerate(csv.reader(f), 1):
 		if len(line) == 2:
 			(rank, tld) = line
 			key = hashlib.md5(tld.encode()).hexdigest()[:3]
@@ -30,18 +30,18 @@ with open("top-1m.csv") as f:
 			crtfile = "certs/%s/%s.der" % (key, tld)
 			entry = Entry(tld = tld, key = key, outfile = outfile, crtfile = crtfile)
 			ctr[key] += 1
-			entries.append(entry)
-			if (max_entries is not None) and (len(ctr) == max_entries):
-				break
+			if (not os.path.isfile(entry.outfile)) and (not os.path.isfile(entry.crtfile)):
+				entries.append(entry)
+		if (max_entries is not None) and (lineno >= max_entries):
+			break
 print("Most common keys:", ctr.most_common(10))
 print("Key directories :", len(ctr))
+print("Entries total   :", len(entries))
 
-def process_entry(entry):
-	if os.path.isfile(entry.outfile) or os.path.isfile(entry.crtfile):
-		print("%s: skipped" % (entry.tld))
-		threads.release()
-		return False
+def log(entry, result, entryid, entrycnt):
+	print("%5.1f%% %6d/%6d %s: %s" % (entryid / entrycnt * 100, entryid, entrycnt, entry.tld, result))
 
+def process_entry(entry, entryid, entrycnt):
 	# Create output file first
 	directory = os.path.dirname(entry.outfile)
 	try:
@@ -55,23 +55,21 @@ def process_entry(entry):
 		try:
 			proc.wait(timeout = 15)
 			if proc.returncode == 0:
-				print("%s: success" % (entry.tld))
+				log(entry, "success", entryid, entrycnt)
 			else:
-				print("%s: failed" % (entry.tld))
+				log(entry, "failed", entryid, entrycnt)
 		except subprocess.TimeoutExpired:
 			proc.kill()
-			print("%s: timed out" % (entry.tld))
+			log(entry, "timed out", entryid, entrycnt)
 	time.sleep(gracetime)
 	threads.release()
 	return True
 
-if randomized:
-	random.shuffle(entries)
-else:
-	entries.sort()
-for entry in entries:
+random.shuffle(entries)
+
+for (entryid, entry) in enumerate(entries):
 	threads.acquire()
-	thread = threading.Thread(target = process_entry, args = (entry, ))
+	thread = threading.Thread(target = process_entry, args = (entry, entryid, len(entries)))
 	thread.start()
 
 for i in range(thread_cnt):
