4b96c92d33e27684d8f5a7c5093b3fbbfd0e6ef3
Johannes Bauer <joe@johannes-bauer.com>
2018-10-14 20:18:23 +0200

    Do not retry failed conversions
    When a cert cannot be converted to DER form (probably because the server
    couldn't be reached), write a zero-length flag-file so that on the next
    run we can skip the conversion outright.

--- cert_extract.py
+++ cert_extract.py
@@ -16,11 +16,16 @@ for (dirname, subdirs, files) in os.walk("raw_certs"):
 		domain = filename[:-4]
 		key = hashlib.md5(domain.encode()).hexdigest()[:3]
 		outfile = "certs/%s/%s.der" % (key, filename[:-4])
-		if os.path.isfile(outfile):
+		inhibitfile = "nocert/%s/%s.der" % (key, filename[:-4])
+		if os.path.isfile(outfile) or os.path.isfile(inhibitfile):
 			continue
 		try:
 			try:
 				os.makedirs(os.path.dirname(outfile))
+			except FileExistsError:
+				pass
+			try:
+				os.makedirs(os.path.dirname(inhibitfile))
 			except FileExistsError:
 				pass
 			x509 = subprocess.check_output([ "openssl", "x509", "-in", fullfilename, "-outform", "der" ])
@@ -28,5 +33,6 @@ for (dirname, subdirs, files) in os.walk("raw_certs"):
 				f.write(x509)
 			successful += 1
 		except subprocess.CalledProcessError:
-			pass
+			with open(inhibitfile, "w") as f:
+				pass
 print("%d new certificates." % (successful))
