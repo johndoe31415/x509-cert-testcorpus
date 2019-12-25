972b758468eb8f9ed407f5dda553ebd4c9324f6a
Johannes Bauer <joe@johannes-bauer.com>
2019-12-25 20:56:44 +0100

    Yet another database format, capable of storing CA certs
    In the previous iteration, we encountered two issues: Firstly, a *lot*
    of certificates are shared. We want to deduplicate them. Additionally,
    we might as well store the CA certificates as well (where a much greater
    amount of duplicates is expected), since we're already transferring them
    in the TLS handshake anyways and right now discard them.

--- CertDB.py
+++ CertDB.py
@@ -3,54 +3,47 @@
 #	Copyright (C) 2018-2019 Johannes Bauer
 #   License: CC-0
 
-import os
 import sqlite3
 import hashlib
 import contextlib
-import collections
 
 class CertDB():
-	_DBEntry = collections.namedtuple("DBEntry", [ "domainname", "fetched_timet", "der_hash_md5", "der_cert" ])
-
 	def __init__(self, sqlite_filename):
 		self._conn = sqlite3.connect(sqlite_filename)
 		self._cursor = self._conn.cursor()
 		with contextlib.suppress(sqlite3.OperationalError):
 			self._cursor.execute("""
 			CREATE TABLE certificates (
-				domainname varchar NOT NULL,
-				fetched_timet integer NOT NULL,
-				der_cert blob NOT NULL,
-				der_hash_md5 blob NOT NULL,
-				PRIMARY KEY(domainname, fetched_timet)
+				cert_sha256 blob PRIMARY KEY,
+				der_cert blob
 			);
 			""")
 
 	def get_all(self):
-		for data in self._cursor.execute("SELECT domainname, fetched_timet, der_hash_md5, der_cert FROM certificates;").fetchall():
-			yield self._DBEntry(*data)
+		for row in self._cursor.execute("SELECT der_cert FROM certificates;").fetchall():
+			yield row[0]
 
-	def get_domainname_timestamps(self):
-		return self._cursor.execute("SELECT domainname, MAX(fetched_timet) FROM certificates GROUP BY domainname;").fetchall()
+	def get_cert(self, cert_sha256):
+		row = self._cursor.execute("SELECT der_cert FROM certificates WHERE cert_sha256 = ?;", (cert_sha256, )).fetchone()
+		if row is not None:
+			return row[0]
 
-	def add_der(self, domainname, fetched_timet, der_cert):
-		der_hash_md5 = hashlib.md5(der_cert).digest()
+	def add_cert(self, der_cert):
+		cert_sha256 = hashlib.sha256(der_cert).digest()
 		with contextlib.suppress(sqlite3.IntegrityError):
-			self._cursor.execute("INSERT INTO certificates (domainname, fetched_timet, der_cert, der_hash_md5) VALUES (?, ?, ?, ?);", (domainname, fetched_timet, der_cert, der_hash_md5))
-
-	def add_der_from_file(self, domainname, der_filename):
-		fetched_timet = round(os.stat(der_filename).st_mtime)
-		with open(der_filename, "rb") as f:
-			der_cert = f.read()
-		self.add_der(domainname, fetched_timet, der_cert)
+			self._cursor.execute("INSERT INTO certificates (cert_sha256, der_cert) VALUES (?, ?);", (cert_sha256, der_cert))
 
 	def commit(self):
 		self._conn.commit()
 
 	def close(self):
+		if self._conn is None:
+			return
 		self.commit()
 		self._cursor.close()
 		self._conn.close()
+		self._cursor = None
+		self._conn = None
 
 	def __del__(self):
 		self.close()
---
+++ CertTOC.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+#	x509-cert-testcorpus - X.509 certificate test corpus
+#	Copyright (C) 2018-2019 Johannes Bauer
+#   License: CC-0
+
+import os
+import sqlite3
+import hashlib
+import contextlib
+import collections
+from CertDB import CertDB
+
+class CertTOC():
+	_Connection = collections.namedtuple("Connection", [ "conn_id", "leaf_only", "fetch_timestamp", "servername", "certs" ])
+
+	def __init__(self, sqlite_filename):
+		self._conn = sqlite3.connect(sqlite_filename)
+		self._cursor = self._conn.cursor()
+		with contextlib.suppress(sqlite3.OperationalError):
+			self._cursor.execute("""
+			CREATE TABLE connections (
+				conn_id integer PRIMARY KEY,
+				leaf_only boolean NOT NULL,
+				fetch_timestamp integer NOT NULL,
+				servername varchar NOT NULL,
+				cert_hashes blob NOT NULL,
+				UNIQUE(servername, fetch_timestamp)
+			);
+			""")
+
+		self._data_dbs = [ CertDB("%s/%02x.sqlite3" % (os.path.dirname(sqlite_filename), i)) for i in range(256) ]
+
+	def get_connection(self, conn_id):
+		row = self._cursor.execute("SELECT leaf_only, fetch_timestamp, servername, cert_hashes FROM connections WHERE conn_id = ?;", (conn_id, )).fetchone()
+		if row is None:
+			return None
+		(leaf_only, fetch_timestamp, servername, cert_hashes) = row
+		cert_hashes = [ cert_hashes[i : i + 32] for i in range(0, len(cert_hashes), 32) ]
+		certs = [ self._get_cert(cert_hash) for cert_hash in cert_hashes ]
+		return self._Connection(conn_id = conn_id, leaf_only = leaf_only, fetch_timestamp = fetch_timestamp, servername = servername, certs = certs)
+
+	def _get_cert(self, cert_hash):
+		dbid = cert_hash[0]
+		cert_db = self._data_dbs[dbid]
+		return cert_db.get_cert(cert_hash)
+
+	def _insert_cert(self, der_cert):
+		cert_hash = hashlib.sha256(der_cert).digest()
+		dbid = cert_hash[0]
+		cert_db = self._data_dbs[dbid]
+		cert_db.add_cert(der_cert)
+		return cert_hash
+
+	def insert_connection(self, servername, fetch_timestamp, certs, leaf_only = False):
+		cert_hashes = [ self._insert_cert(cert) for cert in certs ]
+		cert_hashconcat = b"".join(cert_hashes)
+		with contextlib.suppress(sqlite3.IntegrityError):
+			self._cursor.execute("INSERT INTO connections (leaf_only, fetch_timestamp, servername, cert_hashes) VALUES (?, ?, ?, ?);", (leaf_only, fetch_timestamp, servername, cert_hashconcat))
+
+	def commit(self):
+		for data_db in self._data_dbs:
+			data_db.commit()
+		self._conn.commit()
+
+	def close(self):
+		if self._conn is None:
+			return
+		self.commit()
+		for data_db in self._data_dbs:
+			data_db.close()
+		self._cursor.close()
+		self._conn.close()
+		self._cursor = None
+		self._conn = None
+
+	def __del__(self):
+		self.close()
+
+if __name__ == "__main__":
+	x = CertTOC("test/toc.sqlite3")
+	x.insert_connection("foobar", 1234, [ b"foobar" ])
+	x.insert_connection("foobar", 1235, [ b"foobar second", b"moo" ])
+	print(x.get_connection(2))
+	x.close()
