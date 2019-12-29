a7165d84f1b3e22f7eebb8ad5d4fea21c51acd75
Johannes Bauer <joe@johannes-bauer.com>
2019-12-29 12:46:09 +0100

    Added rebuilding DB script
    If integrity errors occur, this script will rebuild the databases.

---
+++ rebuild_dbs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+#	x509-cert-testcorpus - X.509 certificate test corpus
+#	Copyright (C) 2019-2019 Johannes Bauer
+#   License: CC-0
+
+echo "Really rebuild all Sqlite3 databases (y/n)? "
+read yn
+if [ "$yn" != "Y" ] && [ "$yn" != "y" ]; then
+	echo "Aborted."
+	exit 1
+fi
+
+for SQLITE_DB in certs/*.sqlite3; do
+	if [ ! -f "$SQLITE_DB" ]; then
+		continue
+	fi
+	echo "$SQLITE_DB"
+	rm -f temp_db.sqlite3
+	sqlite3 temp_db.sqlite3 | sqlite3 "$SQLITE_DB" .dump
+	sqlite3 temp_db.sqlite3 "VACUUM;"
+	mv temp_db.sqlite3 "$SQLITE_DB"
+done
