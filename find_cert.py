ea4a04359a4f7726c94c6d3bb2e7d8539fad7934
Johannes Bauer <joe@johannes-bauer.com>
2020-04-30 21:23:30 +0200

    Add ability to quit after a number of findings
    When we're only looking for a specific subset of findings, it can be
    useful to abort after a given number of occurrences. This patch allows
    for that.

--- find_cert.py
+++ find_cert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #	x509-cert-testcorpus - X.509 certificate test corpus
-#	Copyright (C) 2019-2019 Johannes Bauer
+#	Copyright (C) 2019-2020 Johannes Bauer
 #   License: CC-0
 
 import sys
@@ -13,19 +13,23 @@ from FriendlyArgumentParser import FriendlyArgumentParser
 
 parser = FriendlyArgumentParser(description = "Search certificate database for a certificate which contains the proper data.")
 parser.add_argument("-c", "--certdb", metavar = "path", type = str, default = "certs", help = "Specifies the path of the certificate database. Defaults to %(default)s.")
+parser.add_argument("-n", "--nth-match", metavar = "no", type = int, default = 1, help = "Show the n-th match. Defaults to the first match.")
 parser.add_argument("searchstring", help = "Search for this pattern within the OpenSSL text representation of the certificate.")
 args = parser.parse_args(sys.argv[1:])
 
 certdb = CertDatabase(args.certdb)
 cert_count = certdb.certificate_count
 regex = re.compile(args.searchstring, flags = re.MULTILINE | re.IGNORECASE)
+matchno = 0
 for (certno, der_cert) in enumerate(certdb.get_all_certificates()):
 	if (certno % 100) == 0:
 		print("Searching %d of %d (%.1f%%)..." % (certno, cert_count, certno / cert_count * 100))
 	cert_text = subprocess.check_output([ "openssl", "x509", "-inform", "der", "-text", "-noout" ], input = der_cert).decode()
 	if regex.search(cert_text):
-		cert_pem = subprocess.check_output([ "openssl", "x509", "-inform", "der" ], input = der_cert).decode()
-		print(cert_pem)
-		print(cert_text)
-		print("SHA256: %s" % (hashlib.sha256(der_cert).hexdigest()))
-		sys.exit(0)
+		matchno += 1
+		if matchno == args.nth_match:
+			cert_pem = subprocess.check_output([ "openssl", "x509", "-inform", "der" ], input = der_cert).decode()
+			print(cert_pem)
+			print(cert_text)
+			print("SHA256: %s" % (hashlib.sha256(der_cert).hexdigest()))
+			sys.exit(0)
